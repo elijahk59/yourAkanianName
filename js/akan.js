9d468d3f69b5e8ac0a82cfd3757dc63d14afbceb
elijak59 <elijahk59@gmail.com>
2020-05-03 23:43:54 +0300

    added initial images

---
+++ js/akan.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#Birthday").submit(function(event){
+        event.preventDefault();
+        var Birthday=$("input#Date").val();
+        var Gender=$("select#Gender").val();
+        
+        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sataurday'];
+        var MaleNames = ['']
+        var FemaleNames = ['']
+
+    }
+}
