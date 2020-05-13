910f76f0544367f8912f123048ad50e18be63316
elijahk59 <elijahk59@gmail.com>
2020-05-13 20:17:32 +0300

    added js code and formula

--- js/akan.js
+++ js/akan.js
@@ -5,8 +5,65 @@ $(document).ready(function(){
         var Gender=$("select#Gender").val();
         
         var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sataurday'];
-        var MaleNames = ['']
-        var FemaleNames = ['']
+        var MaleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+        var FemaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 
-    }
-}
+        var d = new Date(Birthday);
+        var dayName = days[d.getDay()];
+        var Name = "";
+
+        if (Gender == "Male") {
+            switch (dayName) {
+                case 'Sunday':
+                    Name = MaleNames[0];
+                  break;
+                case 'Monday':
+                    Name = MaleNames[1];
+                  break;
+                case 'Tuesday':
+                    Name = MaleNames[2];
+                  break;
+                case 'Wednesday':
+                    Name = MaleNames[3];
+                  break;
+                case 'Thursday':
+                    Name = MaleNames[4];
+                  break;
+                case 'Friday':
+                    Name = MaleNames[5];
+                  break;
+                case 'Saturday':
+                    Name = MaleNames[6];
+                  break;
+                  default:
+            }
+        }
+        else {
+            switch (dayName) {
+                case 'Sunday' :
+                    Name = FemaleNames[0];
+                    break;
+                case 'Monday' :
+                    Name = FemaleNames[1];
+                    break;
+                case 'Tuesday' :
+                    Name = FemaleNames[2];
+                    break;
+                case 'Wednesday' :
+                    Name = FemaleNames[3];
+                    break;
+                case 'Thursday' :
+                    Name = FemaleNames[4];
+                    break;
+                case 'Friday' :
+                    Name = FemaleNames[5];
+                    break;
+                case 'Saturday' :
+                    Name = FemaleNames[6];
+                    break;
+                    default:
+            }
+        }
+        document.getElementById("name").innerHTML = "Your name in Ghana would have been <strong>"+Name+"</strong> "+" Because you were born on "+dayName;
+    });
+});
--- js/index.js
+++ js/index.js
@@ -1,1 +1,1 @@
-console.log(J.h)
+
